73bf0f00140c7131b56b3f717a4aa05b5a691ebf
Varun Bhardwaj <s.varunbhardwaj@gmail.com>
2018-03-11 18:24:28 +0530

    insertion-sort , gitignore

---
+++ insertion-sort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	scanf("%d",&n);
+	vector<int> a(n);
+	
+	int i,j;
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	
+	for(j=1;j<n;j++) {
+	   	int key=a[j];
+	   	i=j-1;
+	   	while(i>=0 && a[i]> key){
+	   		a[i+1] = a[i];
+	   		i--;
+	   	}
+	   	a[i+1]=key;
+	   }
+	
+	for(i=0;i<n;i++)
+		printf("%d ",a[i]);
+	
+	return 0;
+}
